2e967232417bee5cc13b083eba5d11f7e422b05e
Remon Pel <remon@clearsite.nl>
2021-10-22 13:43:19 +0200

    packaging script now supports a staging function to check before pushing to WordPress.org

--- package.sh
+++ package.sh
@@ -2,14 +2,19 @@
 
 cd "$(dirname "$0")"
 
-[ "" = "$1" ] && echo "Usage: $0 version-number" && echo "Example: $0 1.0.4" && echo "Must match version number in readme.txt, info.json and wp-plugin.php" && exit 999
+[ "" = "$1" ] && echo "Usage: $0 stage|version-number" && echo "use stage to prepare and check the SVN checkout from wp.org" && echo "To release a new version; Example: $0 1.0.4" && echo "Must match version number in readme.txt, info.json and wp-plugin.php" && exit 999
 
 VERSION=$1
 
-[ 1 -gt $(git status | grep 'nothing to commit, working tree clean' | wc -l) ] && echo "GIT checkout not clean, aborting" && exit 1;
-[ 1 -gt $(cat wp-plugin.php | grep Version: | head -n 1 | grep $VERSION | wc -l) ] && echo "Version number given ($VERSION) does not match Version tag in wp-plugin.php" && exit 2;
-[ 1 -gt $(cat info.json | grep '"Version":' | head -n 1 | grep $VERSION | wc -l) ] && echo "Version number given ($VERSION) does not match Version tag in info.json" && exit 3;
-[ 1 -gt $(cat readme.txt | grep 'Stable tag:' | head -n 1 | grep $VERSION | wc -l) ] && echo "Version number given ($VERSION) does not match Stable tag in readme.txt" && exit 4;
+GO=yes
+[ "$1" = "stage" ] && GO=no
+
+if [ "yes" = "$GO" ]; then
+	[ 1 -gt $(git status | grep 'nothing to commit, working tree clean' | wc -l) ] && echo "GIT checkout not clean, aborting" && exit 1;
+	[ 1 -gt $(cat wp-plugin.php | grep Version: | head -n 1 | grep $VERSION | wc -l) ] && echo "Version number given ($VERSION) does not match Version tag in wp-plugin.php" && exit 2;
+	[ 1 -gt $(cat info.json | grep '"Version":' | head -n 1 | grep $VERSION | wc -l) ] && echo "Version number given ($VERSION) does not match Version tag in info.json" && exit 3;
+	[ 1 -gt $(cat readme.txt | grep 'Stable tag:' | head -n 1 | grep $VERSION | wc -l) ] && echo "Version number given ($VERSION) does not match Stable tag in readme.txt" && exit 4;
+fi
 
 SVN_PROJECT=https://plugins.svn.wordpress.org/branded-social-images
 # SVN_PROJECT=svn://svn.clearsite.nl/wp_plugins/__test/branded-social-images
@@ -27,7 +32,10 @@ GIT_DIRECTORY="$(pwd -P)"
 cd ~/Desktop
 
 [ -d BSI_SVN_TRUNK ] && rm -rf ./BSI_SVN_TRUNK
-svn checkout $SVN_PROJECT$SVN_TRUNK BSI_SVN_TRUNK
+svn checkout $SVN_PROJECT$SVN_TRUNK BSI_SVN_TRUNK.$$ 
+mkdir BSI_SVN_TRUNK
+mv BSI_SVN_TRUNK.$$/.svn BSI_SVN_TRUNK/
+rm -rf BSI_SVN_TRUNK.$$
 cd BSI_SVN_TRUNK
 
 SVN_DIRECTORY="$(pwd -P)"
@@ -53,5 +61,11 @@ svn status | grep '?' | awk '{print $2}' | xargs svn add
 # removed or conflicted, how to proceed here?
 svn status | grep '!' | awk '{print $2}' | xargs svn rm
 # remains are modified
-svn commit -m "Import changes from GitHub for version $VERSION"
-svn cp $SVN_PROJECT$SVN_TRUNK $SVN_PROJECT$SVN_TAG -m "version $VERSION"
+
+if [ "yes" = "$GO" ]; then
+	svn commit -m "Import changes from GitHub for version $VERSION"
+	svn cp $SVN_PROJECT$SVN_TRUNK $SVN_PROJECT$SVN_TAG -m "version $VERSION"
+else
+	echo "New SVN checkout is prepared at ~/Desktop/BSI_SVN_TRUNK"
+	svn status
+fi
