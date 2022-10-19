0d02a279ffa4f257d5b4b587048bcba492484136
Remon Pel <remon@acato.nl>
2022-10-19 15:58:53 +0200

    for ARM (M1), node 16 is needed node 16 does not play well with gulp 3, sass module same all kinds of upgrades and fixes to get gulp 4, sass and node 16 running on ARM

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,25 @@ var $ = require('gulp-load-plugins')({
 		rename: {'gulp': 'g'},
 		pattern: ['gulp', 'gulp-*', 'gulp.*', '@*/gulp{-,.}*']
 	}),
+	sass = require('gulp-sass')(require('sass')),
 	webpack = require('webpack'),
 	webpackStream = require('webpack-stream'),
-	webpackConfig = require('./webpack.config.js');
+	webpackConfig = require('./webpack.config.js'),
+	errorHandler = function (err) {
+      console.log(err.message + ' on line ' + err.lineNumber + ' in file : ' + err.fileName);
+    };
 
 // Browsers to target when prefixing CSS.
 var BROWSERS = ['last 2 versions', 'ie >= 9'];
 
 $.g.task('styles', function () {
-	var s = [
+	var r,s = [
 		{source: ['src/*.scss', '!src/_*.scss'], targetDir: 'admin', title: 'Style'}
 	];
 	for (var i in s) {
-		$.g.src(
+		r = $.g.src(
 			s[i].source)
-			.pipe($.sass()).on('error', $.sass.logError)
+			.pipe(sass()).on('error', errorHandler)
 			.pipe($.autoprefixer({browsers: BROWSERS}))
 			.pipe($.g.dest(s[i].targetDir + '/'))
 			.pipe($.cssnano()).pipe($.rename({suffix: '.min'}))
@@ -27,25 +31,27 @@ $.g.task('styles', function () {
 			.pipe($.livereload())
 			.pipe($.notify(s[i].title + ' compiled'));
 	}
+	return r;
 });
 
 $.g.task('scripts', function () {
-	var s = [
+	var r,s = [
 		{source: 'src/admin.js', targetDir: 'admin', title: 'Scripts'}
 	];
 
 	for (var i in s) {
-		$.g.src(
+		r = $.g.src(
 			s[i].source)
 			.pipe($.jshint())
 			.pipe($.jshint.reporter('jshint-stylish'))
-			.pipe(webpackStream(webpackConfig), webpack).on('error', console.log)
+			.pipe(webpackStream(webpackConfig), webpack).on('error', errorHandler)
 			.pipe($.g.dest(s[i].targetDir + '/'))
 			.pipe($.uglify()).pipe($.rename({suffix: '.min'}))
 			.pipe($.g.dest(s[i].targetDir + '/'))
 			.pipe($.livereload())
 			.pipe($.notify(s[i].title + ' compiled'));
 	}
+	return r;
 });
 
 /*-----------------------------------------
@@ -54,8 +60,8 @@ $.g.task('scripts', function () {
 $.g.task('watch', function () {
 	$.livereload.listen();
 
-	$.g.watch('src/**/*.scss', ['styles']);
-	$.g.watch('src/**/*.js', ['scripts']);
+	$.g.watch('src/**/*.scss', $.g.series('styles'));
+	$.g.watch('src/**/*.js', $.g.series('scripts'));
 });
 
-$.g.task('default', ['styles', 'scripts', 'watch']);
+$.g.task('default', $.g.series('styles', 'scripts', 'watch'));
